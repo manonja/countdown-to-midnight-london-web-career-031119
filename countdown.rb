ab33a28d3a73dd4c4a4bca42b35c0668929c80f6
manon jacquin <manon.jacquin@gmail.com>
2019-02-15 12:07:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  count = 0 
-  while count <= number do 
+  while count <= 0 do 
     puts "#{number} SECOND(S)!"
+    number -= 1 
   end
   puts "HAPPY NEW YEAR!"
 end
